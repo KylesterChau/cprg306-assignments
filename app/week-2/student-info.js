de5c486fc2d10c124c56947f1599b70b3faa24a0
Kyle <Kylethompsonchauder@gmail.com>
2025-09-16 16:20:52 -0600

    Linked StudentInfo.js file to pages.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+function StudentInfo() {
+    return (
+        <main>
+            <p>Name: Kyle Chau</p>
+            <p>Github Link: <link>https://github.com/KylesterChau/cprg306-assignments.git</link></p>
+        </main>
+    );
+}
