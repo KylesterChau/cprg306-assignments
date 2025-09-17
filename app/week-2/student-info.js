ccb81131359a17c263dd8ed77cabeb3106746674
Kyle <Kylethompsonchauder@gmail.com>
2025-09-16 20:44:37 -0600

    Fixed minor repilcation error

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ function StudentInfo() {
     return (
         <div>
             <p>Name: Kyle Chau</p>
-            <p>Github Link: <Link href="https://github.com/KylesterChau/cprg306-assignments.git">https://github.com/KylesterChau/cprg306-assignments.git</Link></p>
+            <p>Github: <Link href="https://github.com/KylesterChau/cprg306-assignments.git">https://github.com/KylesterChau/cprg306-assignments.git</Link></p>
         </div>
     );
 }
