5a0a9632093be1a30fc8fff3887e39e3db5a8b10
Kyle <Kylethompsonchauder@gmail.com>
2025-09-26 19:09:13 -0600

    Made text clearer

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,6 +1,7 @@
 import ItemList from "./item-list";
 
 export default function Page() {
+    
     return (
         <main className="flex flex-col items-center bg-gradient-to-br from-gray-900 via-gray-800 to-gray-700 min-h-screen py-10">
             <div className="bg-white/10 backdrop-blur-lg rounded-xl shadow-lg p-8 w-full max-w-md">
