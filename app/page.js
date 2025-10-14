a9366be91740de51d5cf8998f213f1d2f1d86422
Kyle <Kylethompsonchauder@gmail.com>
2025-10-14 14:53:12 -0600

    Completed week 5 frontend

--- app/page.js
+++ app/page.js
@@ -11,8 +11,11 @@ export default function Home() {
         <div>
           <Link href='/week-3'>Week 3</Link>
         </div>
-                <div>
+        <div>
           <Link href='/week-4'>Week 4</Link>
+        </div>
+                <div>
+          <Link href='/week-5'>Week 5</Link>
         </div>
       </div>
     </main>
--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Item from "../week-3/item";
 
 export default function NewItem() {
     const [quantity, setQuantity] = useState(1);
@@ -14,28 +15,41 @@ export default function NewItem() {
 };
 function handleSubmit(event) {
   event.preventDefault();
-  const data = {}
+  const data = {};
+  alert(data);
 };
 
 return (
-        <div>
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required />
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-                <p>Quantity (1-20)</p>
-                <label for="quantity">Current: {quantity}</label>
-            <div className="flex gap-4 mt-4">
-                    <button onClick={decrement} disabled={quantity === 1} className="w-14 h-14 flex items-center justify-center rounded-md border bg-red-600 hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed text-xl font-bold">
-                            -
-                    </button>
-                    <button onClick={increment} disabled={quantity === 20} className="w-14 h-14 flex items-center justify-center rounded-md border bg-green-400 hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed text-xl font-bold">
-                            +
-                    </button>
-            </div>
-                <label for="message">Message:</label>
-                <textarea id="message" name="message"></textarea>
+        <form onSubmit={handleSubmit} className="border-2">
+                <div className="flex flex-col">
+                        <label for="itemName">Item Name</label>
+                        <input type="text" id="name" name="name" className="border-2" required/>
+                </div>
+                <div>
+                        <p>Quantity (1-20)</p>
+                        <label for="quantity">Current: {quantity}</label>
+                        <div className="flex gap-4 mt-4 justify-center">
+                                <button type="button" onClick={decrement} disabled={quantity === 1} className="w-14 h-14 flex items-center justify-center rounded-md border bg-red-600 hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed text-xl font-bold">
+                                        -
+                                </button>
+                                <button type="button" onClick={increment} disabled={quantity === 20} className="w-14 h-14 flex items-center justify-center rounded-md border bg-green-400 hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed text-xl font-bold">
+                                        +
+                                </button>
+                        </div>
+                </div>
+                <div className="">
+                        <label for="category">category:</label>
+                        <select id="category" name="category" className="border-2">
+                                <option value="produce">Produce</option>
+                                <option value="dairy">Dairy</option>
+                                <option value="bakery">Bakery</option>
+                                <option value="meat">Meat</option>
+                                <option value="pantry">Pantry</option>
+                                <option value="frozen">Frozen</option>
+                                <option value="other">Other</option>
+                        </select>
+                </div>
                 <button type="submit">Submit</button>
-        </div>
+        </form>
 );
 }
