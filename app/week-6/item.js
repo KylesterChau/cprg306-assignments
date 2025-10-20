d649c922f7b92000cb47677984acc8b0d4e21e0f
Kyle <Kylethompsonchauder@gmail.com>
2025-10-20 09:41:01 -0600

    Completed part 1 of week 6

---
+++ app/week-6/item.js
@@ -0,0 +1,15 @@
+
+export default function Item({ name, quantity, category }) {
+    const animations = ["animate-bounce", "animate-pulse", "animate-spin", "animate-ping"];
+    let animationType = Math.floor(Math.random() * 4);
+
+    return (
+        <ul>
+            <li  className="border-2 border-gray-700 rounded-lg m-2 p-2 flex flex-col items-center font-sans bg-gray-500 text-white">
+                <p className="">{name}</p>
+                <p>Quantity: {quantity}</p>
+                <p>Category: {category}</p>
+            </li>
+        </ul>
+    );
+}
