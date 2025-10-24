6b9a3f5115a6f31004f071f09a5a34008f952da1
Kyle <Kylethompsonchauder@gmail.com>
2025-10-24 13:07:51 -0600

    Added button animation for item list items

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -3,7 +3,7 @@ export default function Item({ name, quantity, category, onSelect }) {
 
     return (
         <ul>
-            <li onClick={() => onSelect({ name, quantity, category })} className="border-2 border-gray-700 rounded-lg m-2 p-2 flex flex-col items-center font-sans bg-gray-500 text-white">
+            <li onClick={() => onSelect({ name, quantity, category })} className="border-2 border-gray-700 rounded-lg m-2 p-2 flex flex-col items-center font-sans bg-gray-500 text-white hover:cursor-pointer hover:bg-gray-600">
                 <p className="">{name}</p>
                 <p>Quantity: {quantity}</p>
                 <p>Category: {category}</p>
