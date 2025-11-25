e72e4031cff6bcb854fca6ff3a155cb1c036805e
KylesterChau <Kylethompsonchauder@gmail.com>
2025-11-24 21:50:59 -0700

    Fixed vercel issue with import

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -2,16 +2,16 @@ import { db } from "../_utils/firebase";
 import { collection, getDocs, addDoc, query } from "firebase/firestore";
 
 export async function getItems(userId) {
-    const itemsCol = collection(db, "shoppingLists");
-    const query = query(itemsCol);
-    const itemSnapshot = await getDocs(q);
-    const itemList = itemSnapshot.docs
-        .map(doc => ({ id: doc.id, ...doc.data() }))
-        .filter(item => item.userId === userId);
-    return itemList;
+  const itemsCol = collection(db, "shoppingLists");
+  const q = query(itemsCol);
+  const itemSnapshot = await getDocs(q);
+  const itemList = itemSnapshot.docs
+    .map((doc) => ({ id: doc.id, ...doc.data() }))
+    .filter((item) => item.userId === userId);
+  return itemList;
 }
 
 export function addItem(userId, item) {
-    const itemsCol = collection(db, "shoppingLists");
-    return addDoc(itemsCol, { ...item, userId });
+  const itemsCol = collection(db, "shoppingLists");
+  return addDoc(itemsCol, { ...item, userId });
 }
